4d5edca6b06c2ee42c3d9f8486bea0ceebece41d
Hayato Uenohara <uenohara@gmail.com>
2018-06-30 01:22:33 +0900

    Initial commit for blockchain-explorer addition

--- ubuntu1804_hlfv110/hlfsetup.sh
+++ ubuntu1804_hlfv110/hlfsetup.sh
@@ -3,6 +3,5 @@ git clone -b master https://github.com/hyperledger/fabric-samples.git
 cd fabric-samples
 git checkout v1.1.0
 git clone https://github.com/IBM-Blockchain/marbles.git --single-branch --branch v5.0
-cd marbles; sudo npm install gulp -g; cd ..
-cd ..
+cd marbles; sudo npm install gulp -g; npm install; cd ..
 curl -sSL https://goo.gl/PKqygD | bash -s 1.1.0
